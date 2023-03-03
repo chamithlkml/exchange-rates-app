889cd1ac41408a74a2edc66873a25911d39cb384
Chamith Jayaweera <chamithlkml@gmail.com>
2023-03-03 10:18:15 +0000

    Ajusted title

--- resources/views/index/show.blade.php
+++ resources/views/index/show.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>How To Install Vue 3 in Laravel 9 with Vite</title>
+	<title>Exchange Rates</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 	@vite('resources/css/app.css')
 </head>
